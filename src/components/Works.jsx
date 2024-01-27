80d47cccc4a80034c549929229c70fe6b4f22f27
“Riya <rgriya2310@gmail.com>
2024-01-26 21:33:17 -0500

    changed d import

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,7 +6,6 @@ import { github } from '../assets';
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
-import { some } from 'd3';
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return (
