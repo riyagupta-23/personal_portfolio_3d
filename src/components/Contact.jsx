f25e567c2df37f1281db86869bec7ea7cf4b47b0
“Riya <rgriya2310@gmail.com>
2024-01-27 22:56:44 -0500

    with email.js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -122,7 +122,7 @@ const Contact = () => {
           className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary 
           rounded-xl"
           >
-            {loading ? 'Seding...' : 'Sent'}
+            {loading ? 'Seding...' : 'Send'}
 
           </button>
         </form>
